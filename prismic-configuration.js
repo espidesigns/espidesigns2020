b6fe5d1f0cf2c303a7ee1fbeb729fef08d7a73a7
Mark Louie Espedido <espi@espidesigns.com>
2020-01-29 22:59:07 +0800

    Updates prismic config file

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,8 +1,8 @@
 module.exports = {
-  accessToken: 'MC5YTGZpTkJRQUFGWmk1eEp1.77-9UzEf77-977-977-977-977-9We-_ve-_ve-_ve-_ve-_vSo4aO-_ve-_ve-_vQgmSO-_vXLvv71o77-977-9XO-_vQ',
+  accessToken: 'MC5YaHRNRUJVQUFDTUFKbWZo.77-9cu-_ve-_ve-_vRbvv71sHe-_ve-_vVjvv70gKVs977-9B--_ve-_ve-_vXdc77-977-9ZTM1IFLvv70',
 
-  clientId: 'XLfiNBQAAFZi5xJt',
-  clientSecret: '5be7d81c9c1b8ecd0da6d71ba1edf9f5',
+  clientId: 'XhtMEBUAACIAJmfg',
+  clientSecret: 'f3c375902a3d460d634afa32262df70c',
 
-  endpoint: 'https://bruno-arizio.prismic.io/api/v2'
+  endpoint: 'https://espidesigns2020.prismic.io/api/v2'
 }
